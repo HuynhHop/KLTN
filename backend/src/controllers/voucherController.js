866880667e61c54bcf809046196bf9b5e6dc1cb8
hop123 <hovanhuynhhop@gmail.com>
2025-06-15 23:09:08 +0700

    v2

--- backend/src/controllers/voucherController.js
+++ backend/src/controllers/voucherController.js
@@ -34,18 +34,18 @@ class VoucherController {
     const { code, type } = req.query;
 
     try {
-      const vouch = await Voucher.findOne({
-        code,
-        applyTo: type,
-        $or: [
-          { serviceId: null }, // áp dụng toàn bộ
-          { serviceId: id }, // hoặc chỉ áp dụng cho service cụ thể
-        ],
-      });
       // const vouch = await Voucher.findOne({
       //   code,
       //   applyTo: type,
+      //   $or: [
+      //     { serviceId: null }, // áp dụng toàn bộ
+      //     { serviceId: id }, // hoặc chỉ áp dụng cho service cụ thể
+      //   ],
       // });
+      const vouch = await Voucher.findOne({
+        code,
+        applyTo: type,
+      });
 
       if (!vouch || vouch.expiresAt < new Date()) {
         return res.status(404).json({ error: "Invalid or expired" });
